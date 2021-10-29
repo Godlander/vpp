1f7df01afbe11e9f269d50a1e1fdf273733dda5b
Godlander <shizhaowang@ymail.com>
2021-10-28 17:08:36 -0700

    RealMaps

---
+++ RealMaps/assets/minecraft/shaders/include/tools.glsl
@@ -0,0 +1,25 @@
+vec3 rgbhsv(vec3 c)
+{
+    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+    vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+    vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+
+    float d = q.x - min(q.w, q.y);
+    float e = 1.0e-10;
+    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+}
+
+vec3 hsvrgb(vec3 c)
+{
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+bool rougheq(float a, float b) {
+    return (abs(a - b) < 1.);
+}
+
+bool rougheq(vec3 a, vec3 b) {
+    return (rougheq(a.x,b.x) && rougheq(a.y,b.y) && rougheq(a.z,b.z));
+}
